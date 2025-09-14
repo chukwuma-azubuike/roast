aba6c3ff1cb24df657cb3082119310b3ab9c5b1f
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-14 08:20:41 +0100

    feat: 🎸 make notification skeleton loading fuller

--- src/components/NotificationCenter.tsx
+++ src/components/NotificationCenter.tsx
@@ -261,14 +261,16 @@ export function NotificationCenter({ currentUser }: NotificationCenterProps) {
         return (
             <div className="p-4 max-w-4xl mx-auto">
                 <div className="animate-pulse space-y-4">
-                    {[...Array(3)].map((_, i) => (
+                    {[...Array(4)].map((_, i) => (
                         <Card key={i}>
                             <CardContent className="p-4">
-                                <div className="flex items-start space-x-3">
+                                <div className="flex items-start space-x-4">
                                     <div className="w-10 h-10 rounded-lg bg-gray-200" />
-                                    <div className="flex-1">
-                                        <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
+                                    <div className="space-y-4 flex-1">
+                                        <div className="h-5 bg-gray-200 rounded w-3/4" />
                                         <div className="h-3 bg-gray-200 rounded w-1/2" />
+                                        <div className="h-3 bg-gray-200 rounded w-1/2" />
+                                        <div className="h-3 bg-gray-200 rounded w-3/4 " />
                                     </div>
                                 </div>
                             </CardContent>
